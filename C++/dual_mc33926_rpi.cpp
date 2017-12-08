b0037dcc312e8ffd5ffb5d1ec72cf3f19335ee49
Zainkai <turkingtonkevin@gmail.com>
2017-12-07 16:34:24 -0800

    motorControl functions

--- C++/dual_mc33926_rpi.cpp
+++ C++/dual_mc33926_rpi.cpp
@@ -64,6 +64,67 @@ bool Motor::getEnabled(){
     return enabled;
 }
 
+//make inputs an interfact later.
+motorControl::motorControl(
+    int pulsewmPinLeft, int dirPinLeft, int enPinLeft,
+    int pulsewmPinRight, int dirPinRight, int enPinRight,
+){
+    leftMotor = new Motor(pulsewmPinLeft,dirPinLeft,enPinLeft);
+    rightMotor = new Motor(pulsewmPinRight,dirPinRight,enPinRight);
+}
+
+void motorControl::forward(int speed){
+    leftMotor.disable();
+    rightMotor.disable();
+
+    leftMotor.setSpeed(speed,FORWARD);
+    rightMotor.setSpeed(speed,FORWARD);
+
+    leftMotor.enable();
+    rightMotor.enable();
+}
+
+void motorControl::backward(int speed){
+    leftMotor.disable();
+    rightMotor.disable();
+
+    leftMotor.setSpeed(speed,BACKWARD);
+    rightMotor.setSpeed(speed,BACKWARD);
+
+    leftMotor.enable();
+    rightMotor.enable();
+}
+
+void motorControl::left(int speedL,int speedR){
+    leftMotor.disable();
+    rightMotor.disable();
+
+    leftMotor.setSpeed(speedL,BACKWARD);
+    rightMotor.setSpeed(speedR,FORWARD);
+
+    leftMotor.enable();
+    rightMotor.enable();
+}
+
+void motorControl::right(int speedL,int speedR){
+    leftMotor.disable();
+    rightMotor.disable();
+
+    leftMotor.setSpeed(speedL,FORWARD);
+    rightMotor.setSpeed(speedR,BACKWARD);
+
+    leftMotor.enable();
+    rightMotor.enable();
+}
+
+void motorControl::stop(){
+    leftMotor.disable();
+    rightMotor.disable();
+
+    leftMotor.setSpeed(0,FORWARD);
+    rightMotor.setSpeed(0,FORWARD);
+}
+
 int main(int argc, char** argv)
 {
     if(wiringPiSetupGpio() == -1){
